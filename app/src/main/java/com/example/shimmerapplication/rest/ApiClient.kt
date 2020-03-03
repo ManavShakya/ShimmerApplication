eeaa5c76b3ddbee8bfdb00935c469e2dd92d1836
ManavShakya <manavshakya8@gmail.com>
2020-03-03 18:56:22 +0530

    made some naming changes

--- app/src/main/java/com/example/shimmerapplication/rest/ApiClient.kt
+++ app/src/main/java/com/example/shimmerapplication/rest/ApiClient.kt
@@ -1,12 +1,17 @@
 package com.example.shimmerapplication.rest
 
+import com.example.sampleapp.rest.ApiService
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-        private const val url: String = "http://5e510330f2c0d300147c034c.mockapi.io/"
-        fun getClientInstance(): Retrofit = Retrofit.Builder()
-            .baseUrl(url)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+
+        val apiService: ApiService by lazy {
+            val retrofit = Retrofit.Builder()
+                .baseUrl("http://5e510330f2c0d300147c034c.mockapi.io/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            return@lazy retrofit.create(ApiService::class.java)
+        }
+
 }
